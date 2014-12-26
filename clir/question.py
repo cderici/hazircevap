63b5b0bf100f674e80cadf5998234521676f9df4
Çağıl Uluşahin <cagilulusahin@gmail.com>
2014-12-26 13:27:15 +0200

    strip ?

--- clir/question.py
+++ clir/question.py
@@ -5,8 +5,8 @@ class question(object):
     def __init__(self, id,type, tr, eng, answer=None):
         self.id = id
         self.type = type
-        self.tr = tr
-        self.eng = eng
+        self.tr = tr.strip('?')
+        self.eng = eng.strip('?')
         self.answer = answer
         self.clouds = []
 
