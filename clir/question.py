34cb4a89238ce7e909606c8354deb16e726ec782
Çağıl Uluşahin <cagilulusahin@gmail.com>
2014-12-26 11:47:50 +0200

    question class added

---
+++ clir/question.py
@@ -0,0 +1,45 @@
+from search import indriDocFetch, indriHandler, queryBuilder
+from clir import cloud_stats
+import codecs,os
+class question(object):
+    def __init__(self, id,type, tr, eng, answer=None):
+        self.id = id
+        self.type = type
+        self.tr = tr
+        self.eng = eng
+        self.answer = answer
+        self.clouds = []
+
+    def search(self,query_dir = queryBuilder.queryDir):
+        param_filename =  os.path.join(query_dir, str(self.id))
+        try:
+            param_file = open(param_filename,"r")
+        except IOError:
+            queryBuilder.buildIndriQuerySingleFromQuestion(self.id, self.eng)
+            #param_file = open(param_filename,"r")
+        doc_ids = indriHandler.singleIndriQuery(param_filename)
+        doc_dir = os.path.join(query_dir, "docs",  str(self.id))
+
+        for ind,doc_id in enumerate(doc_ids):
+            doc = indriDocFetch.getDoc(doc_id)
+            with codecs.open(os.path.join(doc_dir, str(ind)),'w+', 'utf-8') as q_file:
+                q_file.write(doc)
+
+    def build_word_clouds(self, query_dir = queryBuilder.queryDir):
+        doc_dir = os.path.join(query_dir, "docs",  str(self.id))
+        for _, _, filenames in os.walk(doc_dir):
+            # print path to all filenames.
+            for filename in filenames:
+                doc_filename = os.path.join(doc_dir, filename)
+                text,_ = cloud_stats.parse_document(doc_filename,self.eng)
+                w_c = cloud_stats.calculate_wordcloud(text)
+                self.clouds.append(w_c)
+                #show_wordcloud(w_c)
+
+    def have_answer_in_docs(self):
+        for cloud_ind in range(0,len(self.clouds)):
+            for ind,(word, freq) in enumerate(self.clouds[cloud_ind].words_):
+                if word == self.answer:
+                    print(ind+1,word, freq)
+                    continue
+            print("no")
