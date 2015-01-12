986298329bc36097f7e08538fc9a0abec29abd8e
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-01-12 19:14:23 +0200

    getDocTr added

--- search/indriDocFetch.py
+++ search/indriDocFetch.py
@@ -5,11 +5,23 @@ dumpindex = indriDir + 'dumpindex/dumpindex'
 
 indexDir = indriDir + 'wikipediaIndex/'
 
+indexDir_tr = "/home/hazircevap/turkceIndexDir/"
+
+
 def getDoc(docID):
-    
+
     dump = Popen([dumpindex + ' ' + indexDir + ' dt ' + str(docID)], stdout=PIPE, stderr=PIPE, shell=True)
 
     #print(dumpindex + ' ' + indexDir + ' dt ' + str(docID))
+
+    stdout, stderr = dump.communicate()
+
+    return stdout
+
+def getDocTr(docID):
+    dump = Popen([dumpindex + ' ' + indexDir_tr + ' dt ' + str(docID)], stdout=PIPE, stderr=PIPE, shell=True)
+
+    #print(dumpindex + ' ' + indexDir_tr + ' dt ' + str(docID))
 
     stdout, stderr = dump.communicate()
 
