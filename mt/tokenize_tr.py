099ce802832b39a644a46ccffa5b4f544e74c85f
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-03-03 23:31:27 +0200

    tr file tokenizer added

---
+++ mt/tokenize_tr.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import nltk,codecs,sys,os
+
+def tokenize_tr(in_filename,out_filename):
+    tokens = []
+    with codecs.open(in_filename,"r", 'utf-8') as infile, codecs.open(out_filename,"wb", 'utf-8') as out:
+        for line in infile.readlines():
+            tokens=nltk.word_tokenize(line)
+            print(" ".join(tokens),file=out)
+            #tokens.append("\n")
+            #out.write(" ".join(tokens))
+
+def main(argv):
+    if len(argv) < 3:
+        sys.stderr.write("Usage: %s <filename> infile outfile\n" % (argv[0],))
+        return 1
+
+    #if not os.path.exists(os.path.join(param_dir,str(argv[1]))):
+        #sys.stderr.write("ERROR: File %r was not found in %s!\n" % (argv[1],param_dir))
+    if not os.path.exists(argv[1]):
+        sys.stderr.write("ERROR: File %r was not found!\n" % argv[1])
+        return 1
+    tokenize_tr(argv[1],argv[2])
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
