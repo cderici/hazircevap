0a7b3d4ae0c54f955265f88dba77c890b7e3f528
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-05-18 00:32:07 +0300

    clir completed need to test

--- Main/clirInterface/retrieve_from_english.py
+++ Main/clirInterface/retrieve_from_english.py
@@ -1,13 +1,15 @@
-import urllib2,sys,json
+import urllib2,sys,json,os,io
 from urllib import quote
 
 ### IR #####
 sys.path.append('irInterface')
 from indriHandler import singleIndriQuery
-import queryBuilder
+import queryBuilder,indriDocFetch
+indriDocFetch.indexDir = queryBuilder.index_dir_tr
 ############
 
 key = "AIzaSyCDWvrBXpTFAfbcJqkyaVZrL_AwL2EM2pc"
+translation_dir = "../Data/wikipedia_translations"
 
 def translate(text,source="tr",target="en",domain="google"):
     escaped_source = quote(text.encode('utf8'), '')
@@ -24,11 +26,34 @@ def translate(text,source="tr",target="en",domain="google"):
 def translate_en(text,domain="google"):
     return translate(text,source="en",target="tr")
 
+def fetch_and_translate(doc_id,doc_filename):
+    doc_tuple = indriDocFetch.getDoc(doc_id)
+    doc = " ".join(doc_tuple)
+    translated_doc = []
+    for part in doc.split("\n"):
+        if len(part) < 5000:
+            part_translated = translate_en(part)
+            translated_doc.append(part_translated)
+        else:
+            sys.stderr.write("Doc id %s [HTTP Error 413: Request Entity Too Large]" %doc_id)
+    translated_doc_str = "\n".join(translated_doc)
+    with io.open(doc_filename,"w") as doc_file:
+        doc_file.write(translated_doc_str)
+    return translated_doc_str
+
 def query(question_en):
     paramFile="singleFromWeb_en"
     queryBuilder.buildIndriQuerySingle_en(paramFile, question_en)
-    docs = singleIndriQuery(paramFile, count=3)
-    return docs
+    doc_ids = singleIndriQuery(paramFile, count=3)
+    translated_docs = []
+    for doc_id in doc_ids:
+        doc_filename = os.path.join(translation_dir, doc_id)
+        if os.path.exists(doc_filename):
+            with open(doc_filename) as doc_file:
+                translated_docs.append("".join(doc_file.readlines()))
+        else:
+            translated_docs.append(fetch_and_translate(doc_id,doc_filename))
+    return translated_docs
 
 def main(question_tr):
     question_en = translate(question_tr)
